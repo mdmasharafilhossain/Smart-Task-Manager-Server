096043fb16798d097fcb919032458970e7a7e72e
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-11-20 15:52:19 +0600

    bug fixed in cookie

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
     app.use(cors({
-        origin: "http://localhost:5173",           // or your frontend origin
+        origin: "http://localhost:5173",        
   credentials: true,
     }));
     app.use(express.json()); 
